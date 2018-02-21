ddf2ceb548faf84c007c0879999b42a194c00c86
Ryan Collins <ryan@ohmgeek.co.uk>
2018-02-21 11:22:39 +0000

    Updates

--- deep/deep.py
+++ deep/deep.py
@@ -1,32 +1,40 @@
-import tflearn
-from tflearn.data_utils import to_categorical, pad_sequences, load_csv
-
-# First, load the dataset
-data, labels = load_csv('/home/ryan/Documents/Programming/FakeNewsIdentification/dataset.csv',
-                        target_column=2, categorical_labels=True, n_classes=2)
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+import numpy as np
+import pandas
 
 
-net = tflearn.input_data(shape=[None, 1000])
-net = tflearn.embedding(net, input_dim=10000, output_dim=128)
-net = tflearn.lstm(net, 128, dropout=0.8)
-net = tflearn.fully_connected(net, 2, activation='softmax')
-net = tflearn.regression(net, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
+training_data = pandas.read_csv('../dataset.csv')
+labels = training_data.ix[:0].values.astype('int32')
+X_train = 
 
-model = tflearn.DNN(net, tensorboard_verbose=0)
-model.fit(data, labels, n_epoch=21, validation_set=0.1, show_metrix=True, batch_size=32)
+data_dim = 16
+timesteps = 8
+num_classes = 10
+batch_size = 32
 
+# Expected input batch shape: (batch_size, timesteps, data_dim)
+# Note that we have to provide the full batch_input_shape since the network is stateful.
+# the sample of index i in batch k is the follow-up for the sample i in batch k-1.
+model = Sequential()
+model.add(LSTM(32, return_sequences=True, stateful=True,
+               batch_input_shape=(batch_size, timesteps, data_dim)))
+model.add(LSTM(32, return_sequences=True, stateful=True))
+model.add(LSTM(32, stateful=True))
+model.add(Dense(10, activation='softmax'))
 
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
 
-# Preprocessing
-    # Sequence padding
-    # Use the tflearn pad_sequences function (pads at zero)
+# Generate dummy training data
+x_train = np.random.random((batch_size * 10, timesteps, data_dim))
+y_train = np.random.random((batch_size * 10, num_classes))
 
-# convert labels to binary vectors (2 classes: fake news, or not)
+# Generate dummy validation data
+x_val = np.random.random((batch_size * 3, timesteps, data_dim))
+y_val = np.random.random((batch_size * 3, num_classes))
 
-# Then define the network:
-    ## input layer,
-    ## embedding layer
-    ## lstm layer (dropout can be 0.8 to avoid overfitting)
-    ## fully connected layer - can learn non-linear combinations. use softmax. Yields probabilities
-    ## Regression layer: adam optimizer, categorical crossentropy
-
+model.fit(x_train, y_train,
+          batch_size=batch_size, epochs=5, shuffle=False,
+          validation_data=(x_val, y_val))
