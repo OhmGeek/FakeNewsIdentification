8121fbc65bc8d28d8ba7f5be537416aae34b58c1
Ryan Collins <ryan@ohmgeek.co.uk>
2018-02-22 13:38:38 +0000

    Basic model implemented

--- shallow.py
+++ shallow.py
@@ -14,22 +14,23 @@ class NaiveBayesProcessor(object):
     def train(self, training_data):
 
         parameters = {}
-
-
+        passages = [passage[1] for passage in training_data]
+        target_class = [val[2] for val in training_data]
         self.model = Pipeline([
             ('vect', CountVectorizer()),
             ('tfidf', TfidfTransformer(use_idf=False)),
             ('clf', MultinomialNB()),
         ])
-
+        
         # fit data, using the words, targeting the real/fake
-        self.model.fit(training_data[:][1], training_data[:][2])
-
+        self.model.fit(passages, target_class)
     def get_output(self, input):
         pass
 
     def test_model(self, test_data):
-        return self.model.score(test_data[:][1], test_data[:][2])
+        test_data_passages = [val[1] for val in test_data]
+        test_data_classif = [val[2] for val in test_data]
+        return self.model.score(test_data_passages, test_data_classif)
 def main():
     dp = ShallowDataProcessor()
     filename = 'dataset.csv'
