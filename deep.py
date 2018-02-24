f681b0dc1a6ae6743725fe6af93bf0c59c8aabfd
Ryan Collins <ryan@ohmgeek.co.uk>
2018-02-24 15:05:08 +0000

    TFlearn seems to work, but crashes on my system

--- deep.py
+++ deep.py
@@ -1,5 +1,9 @@
 from shallow.shallow_data_processor import ShallowDataProcessor
 from deep.word_representation import Word2Vec
+import tflearn
+from tflearn.data_utils import to_categorical, pad_sequences
+import numpy as np
+
 def main():
     dp = ShallowDataProcessor()
     filename = 'dataset.csv'
@@ -7,7 +11,23 @@ def main():
     dataset = dp.process()
     
     wtv = Word2Vec()
-    wtv.corpus_to_vec_list(dataset[1][1])
+    
+    train_x = np.array([np.array(wtv.corpus_to_vec_list(row[1])) for row in dataset[1:]])
+    train_y = np.array([row[2] for row in dataset[1:]])
+    print(train_x)
+    print(train_y)
+    max_length = len(train_x[2])
+    train_x = pad_sequences(train_x, max_length, value=0.0)
+    train_y = to_categorical(train_y, nb_classes=2)
+
+    net = tflearn.input_data([None, max_length])
+    net = tflearn.embedding(net, input_dim=10000, output_dim=128)
+    net = tflearn.lstm(net, 128, dropout=0.8)
+    net = tflearn.fully_connected(net, 2, activation='softmax')
+    net = tflearn.regression(net, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
+
+    model = tflearn.DNN(net, tensorboard_verbose=0)
+    model.fit(train_x, train_y, show_metric=True, batch_size=32)
 
 if __name__ == '__main__':
     main()
--- deep/word_representation.py
+++ deep/word_representation.py
@@ -7,11 +7,11 @@ class Word2Vec(object):
         # WORD2VEC pretrained model from http://mccormickml.com/2016/04/12/googles-pretrained-word2vec-model-in-python/
 
         # TODO: load models dynamically using the module path, rather than hard coded.
-        vec_model = gensim.models.KeyedVectors.load_word2vec_format('./deep/model/gnews.bin/data', binary=True)
+        # TODO: remove limit when running on Lexo. Limit is only so that Lino doesn't crash badly.
+        vec_model = gensim.models.KeyedVectors.load_word2vec_format('./deep/model/gnews-slim/data', binary=True, limit=1)
         vec_list = []
         for word in corpus.split(" "):
             # If in vocab, add to list, otherwise ignore.
             if(word in vec_model.vocab):
-                vec_list.append(vec_model[word])
-                print("Word: " + word + " rep: " + str(vec_model[word]))
+                vec_list.extend(vec_model[word])
         return vec_list
