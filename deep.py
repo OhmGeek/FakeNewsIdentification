06c467b88c1df60c41ef38ecd586a6f98f3b1585
Ryan Collins <ryan@ohmgeek.co.uk>
2018-03-07 10:24:44 +0000

    Fixed some speed issues

--- deep.py
+++ deep.py
@@ -8,28 +8,29 @@ from keras.layers import Dropout
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.utils import to_categorical
-
+from keras.optimizers import Adam
 def main():
     dp = ShallowDataProcessor()
     filename = 'dataset.csv'
     dp.read_dataset_from_file(filename)
     dataset = dp.process()
-    pivot = int(0.6 * len(dataset))
+    max_data=100
+    pivot = int(0.6 * max_data)
     wtv = Word2Vec()
     print("Start training set x")
     train_x = np.array([np.array(wtv.corpus_to_vec_list(row[1])) for row in dataset[1:pivot]])
+    print(train_x)
     print("Now training set y")
     train_y = np.array([row[2] for row in dataset[1:pivot]])
     print("Now test set x")
-    test_x = np.array([np.array(wtv.corpus_to_vec_list(row[1])) for row in dataset[pivot+1:]])
+    test_x = np.array([np.array(wtv.corpus_to_vec_list(row[1])) for row in dataset[pivot+1:max_data]])
     print("Now test set y")
-    test_y = np.array([row[2] for row in dataset[pivot+1:]])
+    test_y = np.array([row[2] for row in dataset[pivot+1:max_data]])
 
     print("Train set X: ")
 
 
     max_length = 300
-
     train_x = sequence.pad_sequences(train_x, maxlen=max_length)
     train_y = to_categorical(train_y, num_classes=2)
 
@@ -42,10 +43,12 @@ def main():
     model = Sequential()
     model.add(Embedding(input_dim=max_length, output_dim=1000, input_length=300))
     model.add(Dropout(0.1))
-    model.add(LSTM(120))
+    model.add(LSTM(200))
     model.add(Dropout(0.1))
-    model.add(Dense(2, activation='sigmoid'))
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+    model.add(Dense(500, activation='sigmoid'))
+    model.add(Dense(2, activation='softmax'))
+    adam = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=True)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
     # print(model.summary())
     model.fit(train_x, train_y, epochs=100, batch_size=32)
     score = model.evaluate(test_x, test_y, verbose=1)
--- deep/word_representation.py
+++ deep/word_representation.py
@@ -2,30 +2,13 @@ import gensim
 import numpy as np
 class Word2Vec(object):
     def __init__(self):
-        pass
+        self.vec_model = gensim.models.KeyedVectors.load_word2vec_format('./deep/model/gnews-slim/data', binary=True)
     def corpus_to_vec_list(self, corpus):
         # WORD2VEC pretrained model from http://mccormickml.com/2016/04/12/googles-pretrained-word2vec-model-in-python/
 
         # TODO: load models dynamically using the module path, rather than hard coded.
         # TODO: remove limit when running on Lexo. Limit is only so that Lino doesn't crash badly.
-        vec_model = gensim.models.KeyedVectors.load_word2vec_format('./deep/model/gnews-slim/data', binary=True)
         first = True
         vec_list = None
         word_counter = 0
-        return np.nanmean([vec_model[word] for word in corpus.split(" ") if word in vec_model.vocab], dtype=np.float64, axis=0)
-        # for word in corpus.split(" "):
-        #     # If in vocab, add to list, otherwise ignore.
-        #     if(word in vec_model.vocab):
-        #         word_counter += 1.0
-        #         arr = np.array(vec_model[word])
-        #         arr = arr.astype(np.float64)
-        #         arr.setflags(write=1)
-        #         if(first):
-        #             vec_list = arr
-        #         else:
-        #             vec_list = np.add(vec_list, arr)
-        #
-        # if(vec_list is None):
-        #     return np.array([])
-        # vec_list = np.divide(vec_list, word_counter)
-        # return vec_list
+        return np.nanmean([self.vec_model[word] for word in corpus.split(" ") if word in self.vec_model.vocab], dtype=np.float64, axis=0)
