a6739b356a54ff875a60252f36064331771100a5
Ryan Collins <ryan@ohmgeek.co.uk>
2018-03-10 17:05:34 +0000

    Gotten a better model

--- deep.py
+++ deep.py
@@ -16,25 +16,29 @@ def main():
     dp.read_dataset_from_file(filename)
     stopword_list = ["I" , "a" , "about" ,"an" ,"are" ,"as" ,"at" ,"be" ,"by" ,"com" ,"for" ,"from","how","in" ,"is" ,"it" ,"of" ,"on" ,"or","that","the" ,"this","to" ,"was" ,"what" ,"when","where","who" ,"will","with","the","www"]
     dataset = dp.process(stopwords=stopword_list)
-    max_data=1000
+    max_data=len(dataset)
     pivot = int(0.6 * max_data)
 
 
     # First, we are going to tokenize the dataset.
     # Then convert to sequences
     # Finally we will create an embedding from the word2vec method.
-    tokenizer = Tokenizer(num_words=10000)
+    tokenizer = Tokenizer(num_words=15000)
     tokenizer.fit_on_texts([data[1] for data in dataset])
     sequences = tokenizer.texts_to_sequences([data[1] for data in dataset[1:]])
     word_index = tokenizer.word_index
     print("Found " + str(word_index) + " unique tokens")
 
-    train_x = sequence.pad_sequences(sequences, maxlen=1000)
+    data_x = sequence.pad_sequences(sequences, maxlen=1000)
 
     labels = [int(doc[2]) for doc in dataset[1:]]
-    train_y = to_categorical(labels) # Labels
+    data_y = to_categorical(labels) # Labels
 
+    train_x = data_x[:pivot]
+    train_y = data_y[:pivot]
 
+    test_x = data_x[pivot+1:]
+    test_y = data_y[pivot+1:]
     # Now deal with embeddings, using word2vec
     word2vec = Word2Vec()
     embedding_matrix = np.zeros((len(word_index) + 1, 300))
@@ -54,14 +58,18 @@ def main():
                             input_length=1000,
                             trainable=False))
     model.add(Dropout(0.1))
+    model.add(LSTM(200, return_sequences=True))
+    model.add(Dropout(0.1))
     model.add(LSTM(100))
     model.add(Dropout(0.1))
+    model.add(Dense(100, activation='tanh'))
     model.add(Dense(2, activation='softmax'))
     adam = Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=True)
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
     # print(model.summary())
-    model.fit(train_x, train_y, epochs=100, batch_size=32)
+    model.fit(train_x, train_y, epochs=5, batch_size=32, validation_data=(test_x, test_y))
     score = model.evaluate(test_x, test_y, verbose=1)
     print("Final Accuracy is: " + str(score))
+    model.save('deep-model.h5')
 if __name__ == '__main__':
     main()
--- deep/lstm_network.py
+++ deep/lstm_network.py
@@ -4,25 +4,14 @@ import numpy as np
 
 class LSTMNetwork(object):
     def __init__(self):
-        pass
+        
 
     def train(self):
-        train_x = pad_sequences(train_x, max_length, value=0.0)
-        train_y = to_categorical(train_y, nb_classes=2)
-
-        net = tflearn.input_data([None, max_length])
-        net = tflearn.embedding(net, input_dim=10000, output_dim=128)
-        net = tflearn.lstm(net, 128, dropout=0.8)
-        net = tflearn.fully_connected(net, 2, activation='softmax')
-        net = tflearn.regression(net, optimizer='adam', learning_rate=0.0001, loss='categorical_crossentropy')
-
-        model = tflearn.DNN(net, tensorboard_verbose=0)
-        model.fit(train_x, train_y, show_metric=True, batch_size=32)
-
-
-    def evalulate(self):
         pass
 
-    def get_output(self):
+    def evaluate(self):
+        pass
+
+    def save(self):
         pass
     
